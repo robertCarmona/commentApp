7ed3072173567c968ed985bb203cac247fc1c954
Robert Carmona <Robert@Roberts-MBP-2.home>
2018-10-18 18:18:16 -0400

    finish project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 console.log("connected");
+
+// gets name and users comment
+var userN = document.getElementById("name");
+var userC = document.getElementById("comment");
+
+function submitC(){
+  if(userN.value == "" || userC.value == ""){
+    console.log("Field is missing");
+    window.alert("Your missing Info Try Again");
+
+  }
+  else{
+    // creats a container for comments
+    var commentBox = document.createElement("section");
+    commentBox.className = "container-fluid mx-auto cBox";
+
+    // puts the container in the body
+    document.body.appendChild(commentBox);
+
+    // creates a box inside align as well
+    var commentHolder = document.createElement("div");
+    commentHolder.className = "col-7 mx-auto cHolder";
+    commentBox.appendChild(commentHolder);
+
+    // creates an h4 and p element inside the box
+    var cH4 = document.createElement("h4");
+    var cPar = document.createElement("p");
+    cH4.innerText = userN.value;
+    cPar.innerText = userC.value;
+    commentHolder.appendChild(cH4);
+    commentHolder.appendChild(cPar);
+    console.log("comment Added");
+  }
+};
